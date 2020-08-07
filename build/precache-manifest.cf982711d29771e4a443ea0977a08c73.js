f667f2ce3d4737ddea9375febf6755a8088473e5
Saravanakumar T N <37114252+Saravanakumar-TN-au5@users.noreply.github.com>
2020-08-07 19:59:07 +0530

    Initial

---
+++ build/precache-manifest.cf982711d29771e4a443ea0977a08c73.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2d35e517a968f31d31f117dc062e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d4fcd4c323a8aa468c",
+    "url": "/static/css/main.89fd15b5.chunk.css"
+  },
+  {
+    "revision": "60244c735bef6e0d2e89",
+    "url": "/static/js/2.c18dc45b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c18dc45b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d4fcd4c323a8aa468c",
+    "url": "/static/js/main.c9406f72.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "6df9306641bb98c55920f9154b7eb1b0",
+    "url": "/static/media/arrow-left.6df93066.svg"
+  },
+  {
+    "revision": "e0b4d24bd396acf71c970fa375bda4e9",
+    "url": "/static/media/arrow-right.e0b4d24b.svg"
+  },
+  {
+    "revision": "af600b4ba404fa850771aa1e22f21c5e",
+    "url": "/static/media/arrowUp.af600b4b.svg"
+  },
+  {
+    "revision": "1cb922062b1bdadcafa7d6022b015daf",
+    "url": "/static/media/arrrowDown.1cb92206.svg"
+  },
+  {
+    "revision": "08bab3261bbc942a78ee037dd7d67264",
+    "url": "/static/media/bag-fill.08bab326.svg"
+  },
+  {
+    "revision": "25eed769b5146e6958c19b3f16a695a0",
+    "url": "/static/media/bag-new.25eed769.svg"
+  },
+  {
+    "revision": "0f0a056e8d5201f34189fc50f0566f97",
+    "url": "/static/media/heart-fill.0f0a056e.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/static/media/heart-new.7746f5e4.svg"
+  },
+  {
+    "revision": "cc25b4fa438ef581f403c5609a97d5d0",
+    "url": "/static/media/heart-new.cc25b4fa.svg"
+  },
+  {
+    "revision": "5f89f0c3786954f35d758efba8d1d644",
+    "url": "/static/media/men-bag.5f89f0c3.webp"
+  },
+  {
+    "revision": "040474de539c44e55fe946cbfe47ef4c",
+    "url": "/static/media/men-shoe.040474de.webp"
+  },
+  {
+    "revision": "d922027a89e606388aa44da0b14c1ca3",
+    "url": "/static/media/men-wear.d922027a.webp"
+  },
+  {
+    "revision": "b2feb1c702b9b4c46343cfc222770c0d",
+    "url": "/static/media/search-new.b2feb1c7.svg"
+  },
+  {
+    "revision": "60b4d3ecf56b5d98a5e57087f10edffb",
+    "url": "/static/media/user-fill.60b4d3ec.svg"
+  },
+  {
+    "revision": "5683ec7cd0cf51f0dbea65253fa16da3",
+    "url": "/static/media/user-new.5683ec7c.svg"
+  },
+  {
+    "revision": "f12d8cdf3e5e12e6c64d32659a889e61",
+    "url": "/static/media/women-bag.f12d8cdf.webp"
+  },
+  {
+    "revision": "5fe444e93a2b055a82055669aa6bf4aa",
+    "url": "/static/media/women-shoe.5fe444e9.webp"
+  },
+  {
+    "revision": "fdf67345ff9173d3a2f23739b3a2b871",
+    "url": "/static/media/women-wear.fdf67345.webp"
+  }
+]);
